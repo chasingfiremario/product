4a942a39f2c16f3a33a9a37a2a53ccf474d683d5
Mario Rossi <chasingfiremario@gmail.com>
2023-12-28 10:11:31 +0100

    Add files via upload

---
+++ ProductDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_PRODUCT_BY_ID } from './allProductsQuery.graphql'; // Import your GraphQL query
+
+const ProductDetail = () => {
+  // Use the query to fetch and display a single product
+};
+export default ProductDetail;
