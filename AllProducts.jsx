79428a97a80b67eab08fc09442c4db04f02f06b3
Mario Rossi <chasingfiremario@gmail.com>
2023-12-28 10:11:21 +0100

    Add files via upload

---
+++ AllProducts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_PRODUCTS } from './allProductsQuery.graphql'; // Import your GraphQL query
+
+const AllProducts = () => {
+  // Use the query and render the component accordingly
+};
+export default AllProducts;
